19f4b436cbe049158ffe7982e7f4091f8ceb6d68
AnnaVitkina <anna_vitkina@trimble.com>
2026-08-31 10:06:22 +0200

    Update extract_previous_rate_card.py

--- extract_previous_rate_card.py
+++ extract_previous_rate_card.py
@@ -9,7 +9,7 @@ from openpyxl import load_workbook
 from cost_utils import extract_container_from_cost_title, merge_metrics, normalize_cost_title, sort_metrics
 
 
-from paths import INPUT_PREVIOUS_DIR, PROCESSING_DIR
+from cat_paths import INPUT_PREVIOUS_DIR, PROCESSING_DIR
 
 INPUT_DIR = INPUT_PREVIOUS_DIR
 OUTPUT_DIR = PROCESSING_DIR
